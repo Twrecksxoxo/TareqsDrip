38391e81664c7234eaa806201518f0b3ac40cb77
GoCat Dev <dev@gocart.local>
2025-12-03 09:35:59 +0600

    Fix: Inngest functions syntax errors (imports, typos, string templates)

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,13 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
+import { syncUserCreation, syncUserUpdation, syncUserDeletion } from "../../../inngest/functions";
 
-// Create an API that serves zero functions
+// Create an API that serves Inngest functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     syncUserCreation,
-    syncUSerUpdation,
+    syncUserUpdation,
     syncUserDeletion
   ],
 });
